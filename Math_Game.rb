9b2a00ef79ff1d1e9db610d960f48b71c58e2bad
smpatric <patrick.cody.stewart@gmail.com>
2015-03-06 03:34:35 +0000

    Added stat_check method

--- Math_Game.rb
+++ Math_Game.rb
@@ -12,18 +12,13 @@ def player1_question_generator
     puts "Awesome! You're a math wiz! +1 for you!"
     @player_1_score = @player_1_score + 1
     puts "Your score is #{@player_1_score}"
+    player2_question_generator
   else
     puts "Wow, you suck."
     @player_1_lives = @player_1_lives - 1
     puts "You have #{@player_1_lives} left."
+    player2_question_generator
   end
-  # if @player_1_lives == 0
-  #   puts "The game has ended!"
-  # elsif @player_1_score = 4
-  #   puts "You win!"
-  # else
-  #   question_generator
-  # end
 end
 
 def player2_question_generator
@@ -35,10 +30,34 @@ def player2_question_generator
     puts "Awesome! You're a math wiz! +1 for you!"
     @player_2_score = @player_2_score + 1
     puts "Your score is #{@player_2_score}"
+    player1_question_generator
   else
     puts "Wow, you suck."
     @player_2_lives = @player_2_lives - 1
     puts "You have #{@player_2_lives} left."
+    player1_question_generator
   end
 
-question_generator
+def stats_check
+  if (@player_1_lives || @player_2_lives) == 0
+    if @player_1_lives == 0
+      puts "Player 1 lost all lives! Player 2 wins!"
+    elsif @player_2_lives == 0
+      puts "Player 2 lost all lives! Player 1 wins!"
+    elsif @player_1_score == 20
+      puts "Player 1 wins #{player_1_score} to #{player_2_score}!"
+    elsif @player_2_score == 20
+      puts "Player 1 wins #{player_1_score} to #{player_2_score}!"     
+    else
+      next
+    end
+  end
+end
+        
+
+
+
+
+
+
+
