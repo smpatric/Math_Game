06821e76a8acea9c5ffac318445cb61284c85e7c
smpatric <patrick.cody.stewart@gmail.com>
2015-03-06 22:37:11 +0000

    Created game_logic method and added game continuation and colorization"

--- Math_Game.rb
+++ Math_Game.rb
@@ -1,3 +1,4 @@
+require 'colorize'
 
 class Player
   attr_accessor(:name, :score, :lives)
@@ -23,36 +24,50 @@ def questions(player)
   answer = gets.chomp
   if answer.to_i == num1 + num2
     player.add_points
-    puts "Correct! Your score is #{player.score}"
+    puts "Correct! Your score is #{player.score}".colorize(:green)
   else
     player.lose_life
-    puts "Nope! You Suck! You have #{player.lives} lives left!"
+    puts "Nope! You Suck! You have #{player.lives} lives left!".colorize(:red)
   end
 end
 
-def game
-  puts "Player 1, what's your name?: "
-  player_1_name = gets.chomp
-  player_1 = Player.new(player_1_name)
-  puts "Player 2, what's your name?: "
-  player_2_name = gets.chomp
-  player_2 = Player.new(player_2_name) 
+def game_logic
   who_is_playing = 0 
-  while (player_1.lives > 0  && player_2.lives > 0 )
+  while (@player_1.lives > 0  && @player_2.lives > 0 )
     if who_is_playing == 0
-      questions(player_1)
+      questions(@player_1)
       who_is_playing = 1
     else
-      questions(player_2)
+      questions(@player_2)
       who_is_playing = 0
     end
   end
   puts "Game Over!"
-  if player_2.lives.to_i == 0
-    puts "#{player_1.name} wins! Your score was #{player_1.score}!"
+  if @player_2.lives.to_i == 0
+    puts "#{@player_1.name} wins! The score is #{@player_1.name}: #{@player_1.score} to #{@player_2.name}: #{@player_2.score}!"
   else
-    puts "#{player_2.name} wins! Your score was #{player_2.score}!"
+    puts "#{@player_2.name} wins! The score is #{@player_2.name}: #{@player_2.score} to #{@player_1.name}: #{@player_1.score}!"
   end
+  puts "Would you like to play again? Yes/No".colorize(:yellow)
+  again = gets.chomp.downcase
+  if again == "yes"
+    @player_1.lives && @player_2.lives = 3
+    who_is_playing = 0
+    game_logic
+  else
+    puts "Thanks for playing!".colorize(:light_green)
+  end
+end
+
+
+def game
+  puts "Player 1, what's your name?: "
+  player_1_name = gets.chomp
+  @player_1 = Player.new(player_1_name)
+  puts "Player 2, what's your name?: "
+  player_2_name = gets.chomp
+  @player_2 = Player.new(player_2_name) 
+  game_logic
 end
 
 game
